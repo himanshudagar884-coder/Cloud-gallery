37b2a16864648bb0a80d196f005bb2818529b5e8
himanshudagar884-coder <himanshudagar884@gmail.com>
2026-05-10 12:13:03 +0530

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,1 +1,22 @@
+const CLOUD_NAME = "dlf3zxgkp";
+const UPLOAD_PRESET = "cloudgallery_upload";
 
+export async function uploadToCloudinary(file){
+
+  const formData = new FormData();
+
+  formData.append("file", file);
+  formData.append("upload_preset", UPLOAD_PRESET);
+
+  const response = await fetch(
+    `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/auto/upload`,
+    {
+      method:"POST",
+      body:formData
+    }
+  );
+
+  const data = await response.json();
+
+  return data.secure_url;
+}
