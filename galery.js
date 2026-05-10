4c82d95bf8bff1a4d4e24260e25752ee974b1e7d
himanshudagar884-coder <himanshudagar884@gmail.com>
2026-05-10 12:20:35 +0530

    Update galery.js

--- galery.js
+++ galery.js
@@ -1,1 +1,71 @@
+import { auth, db } from "./firebase.js";
+import { uploadToCloudinary } from "./upload.js";
 
+import {
+  ref,
+  push,
+  set,
+  onValue
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const gallery = document.getElementById("gallery");
+
+let currentUid = "";
+
+auth.onAuthStateChanged(user => {
+
+  if(!user){
+    location.href = "login.html";
+    return;
+  }
+
+  currentUid = user.uid;
+
+  loadGallery();
+});
+
+window.uploadMedia = async function(){
+
+  const file = document.getElementById("fileInput").files[0];
+
+  if(!file) return;
+
+  const url = await uploadToCloudinary(file);
+
+  await set(
+    push(ref(db, "Users/" + currentUid + "/Gallery")),
+    {
+      url
+    }
+  );
+};
+
+function loadGallery(){
+
+  onValue(
+    ref(db, "Users/" + currentUid + "/Gallery"),
+    snapshot => {
+
+      gallery.innerHTML = "";
+
+      snapshot.forEach(child => {
+
+        const data = child.val();
+
+        const imageUrl = typeof data === "string"
+          ? data
+          : data.url;
+
+        if(!imageUrl) return;
+
+        const img = document.createElement("img");
+
+        img.src = imageUrl;
+        img.className = "media";
+
+        gallery.appendChild(img);
+      });
+
+    }
+  );
+}
