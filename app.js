c1cdb01c40ce5bb4a8b6b2528d8bad5501e9fd38
himanshudagar884-coder <himanshudagar884@gmail.com>
2026-05-10 12:14:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+import { auth, db } from "./firebase.js";
 
+import {
+  ref,
+  update
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+auth.onAuthStateChanged(user => {
+
+  if(user){
+
+    const userRef = ref(db, "Users/" + user.uid);
+
+    update(userRef, {
+      online:true
+    });
+
+    window.addEventListener("beforeunload", async () => {
+
+      await update(userRef, {
+        online:false
+      });
+
+    });
+  }
+
+});
